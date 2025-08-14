e225c6fe9d6503e260f545380af8b12f68967422
Hassan Raza <31651864+Eng-HassanRaza@users.noreply.github.com>
2025-08-14 15:46:13 +0200

    v17

--- shared/src/commonMain/kotlin/com/learningpartner/shared/domain/models/LanguageLevel.kt
+++ shared/src/commonMain/kotlin/com/learningpartner/shared/domain/models/LanguageLevel.kt
@@ -1,7 +1,6 @@
 package com.learningpartner.shared.domain.models
 
 import kotlinx.serialization.SerialName
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
