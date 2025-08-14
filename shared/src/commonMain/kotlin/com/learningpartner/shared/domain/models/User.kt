2b8488fbf22110e8cb17eef6961f091b48b07b99
Hassan Raza <31651864+Eng-HassanRaza@users.noreply.github.com>
2025-08-14 14:33:03 +0200

    v10

--- shared/src/commonMain/kotlin/com/learningpartner/shared/domain/models/User.kt
+++ shared/src/commonMain/kotlin/com/learningpartner/shared/domain/models/User.kt
@@ -1,15 +1,20 @@
 package com.learningpartner.shared.domain.models
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
 
 @Serializable
 data class User(
     val id: Int,
     val username: String,
     val email: String,
+    @SerialName("first_name")
     val firstName: String,
+    @SerialName("last_name")
     val lastName: String,
+    @SerialName("date_joined")
     val dateJoined: String? = null,
+    @SerialName("user_type")
     val userType: String? = null
 ) {
     val fullName: String
